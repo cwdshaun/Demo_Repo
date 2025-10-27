abd80b4da6ac6b68bc94e658eb6b52845f86acef
cwdshaun <shaun.afzal@commercialwaterdistributing.com>
2025-10-27 16:47:53 +0500

    Updated calculator.py with new features

--- calculator.py
+++ calculator.py
@@ -1,14 +1,27 @@
+print("---------------------------")
 print("Hello THis is Calculator")
-user_input=input("Enter Operation you want to Perform i.e +,-,/,x,^  :")
-num1=int(input("Please Input First Number   :"))
-num2=int(input("Please Input Second Number   :"))
-if(user_input=="+"):
-    print("Sum of ",num1,"and ",num2," is ",num1+num2)
-elif(user_input=="-"):
-    print("Subtraction of ",num1,"and ",num2," is ",num1-num2)
-elif(user_input=="^"):
-    print(num2," As Power of ",num1," is ",num1**num2)
-elif(user_input=="x"):
-    print("Multiplication of ",num1,"and ",num2," is ",num1*num2)
-else:
-    print("Division of ", num1, "/ ", num2, " is ", num1/num2)
+print("---------------------------")
+user_input=""
+while(user_input!="Q"):
+    user_input = input("Enter Operation you want to Perform i.e +,-,/,x,^,Sqrt Or Q to Quit :")
+    if (user_input == "Sqrt"):
+        num = int(input("Please Input The Number   :"))
+        print("Square Root of ", num, "is", num ** 0.5)
+    elif(user_input=="Q"):
+        break
+    else:
+        num1 = int(input("Please Input First Number   :"))
+        num2 = int(input("Please Input Second Number   :"))
+        if (user_input == "+"):
+            print("Sum of ", num1, "and ", num2, " is ", num1 + num2)
+        elif (user_input == "-"):
+            print("Subtraction of ", num1, "and ", num2, " is ", num1 - num2)
+        elif (user_input == "^"):
+            print(num2, " As Power of ", num1, " is ", num1 ** num2)
+        elif (user_input == "x"):
+            print("Multiplication of ", num1, "and ", num2, " is ", num1 * num2)
+        else:
+            print("Division of ", num1, "/ ", num2, " is ", num1 / num2)
+print("---------------------------")
+print("Thank You Very Much Bye Allah Hafiz")
+print("---------------------------")
